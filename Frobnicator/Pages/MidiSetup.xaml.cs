57e58acb3bb5097a2634fe733841ae05bc9f0c71
John Stovin <john.d.stovin@gmail.com>
2017-02-10 10:06:37 +0000

    MIDI Setup pages and bindings

---
+++ Frobnicator/Pages/MidiSetup.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+using Frobnicator.ViewModels;
+
+namespace Frobnicator.Pages
+{
+   public partial class MidiSetup : UserControl
+   {
+      public MidiSetup()
+      {
+         InitializeComponent();
+
+         DataContext = new MidiSetupViewModel();
+      }
+   }
+}
---
+++ Frobnicator/ViewModels/MidiSetupViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Frobnicator.ViewModels
+{
+   public class MidiSetupViewModel
+   {
+      public IEnumerable<string> DeviceNames { get; } = new[] {"Device1", "Device2"};
+   }
+}
