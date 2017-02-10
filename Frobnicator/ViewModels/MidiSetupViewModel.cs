edcddeac9302da3fcd9f35bf4919bf11d8e34547
John Stovin <john.d.stovin@gmail.com>
2017-02-10 11:03:56 +0000

    UI now gets MIDI Input info from F# lib via ViewModel

--- Frobnicator/ViewModels/MidiSetupViewModel.cs
+++ Frobnicator/ViewModels/MidiSetupViewModel.cs
@@ -1,9 +1,44 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Frobnicator.Annotations;
 
 namespace Frobnicator.ViewModels
 {
-   public class MidiSetupViewModel
+   public class MidiSetupViewModel : INotifyPropertyChanged
    {
-      public IEnumerable<string> DeviceNames { get; } = new[] {"Device1", "Device2"};
+      public IEnumerable<string> DeviceNames => MidiInput.deviceNames;
+
+      private int selectedItem;
+
+      public int SelectedItem
+      {
+         get { return selectedItem; }
+         set
+         {
+            selectedItem = value;
+            OnPropertyChanged(nameof(SelectedItem));
+            OnPropertyChanged(nameof(SelectedManufacturer));
+            OnPropertyChanged(nameof(SelectedProductId));
+         }
+      }
+
+      public string SelectedManufacturer
+      {
+         get { return SelectedItem < 0 ? string.Empty : MidiInput.manufacturers[SelectedItem]; }
+      }
+
+      public string SelectedProductId
+      {
+         get { return SelectedItem < 0 ? string.Empty : MidiInput.productIds[SelectedItem].ToString(); }
+      }
+
+      public event PropertyChangedEventHandler PropertyChanged;
+
+      [NotifyPropertyChangedInvocator]
+      protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+      {
+         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+      }
    }
 }
---
+++ StreamAudio/MidiInput.fs
@@ -0,0 +1,19 @@
+﻿module MidiInput
+
+open NAudio.Midi
+open System
+
+let deviceInfo =
+    let deviceCount = MidiIn.NumberOfDevices
+    match deviceCount with 
+    | 0 -> []
+    | _ -> [0..(deviceCount-1)] |> List.map MidiIn.DeviceInfo
+
+let deviceNames =
+    deviceInfo |> List.map (fun info -> info.ProductName)
+
+let manufacturers =
+    deviceInfo |> List.map (fun info -> info.Manufacturer.ToString())
+
+let productIds =
+    deviceInfo |> List.map (fun info -> info.ProductId)
