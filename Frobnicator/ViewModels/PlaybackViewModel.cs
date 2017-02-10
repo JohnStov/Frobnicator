d0e95b86c865e4c6c89d505ac0f64012a7a14123
John Stovin <john.d.stovin@gmail.com>
2017-02-10 14:52:12 +0000

    More UI

---
+++ Frobnicator/ViewModels/PlaybackViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Input;
+using FirstFloor.ModernUI.Presentation;
+using Frobnicator.Annotations;
+
+namespace Frobnicator.ViewModels
+{
+   public class PlaybackViewModel : INotifyPropertyChanged
+   {
+      public event PropertyChangedEventHandler PropertyChanged;
+
+      public ICommand StartCommand { get; } = new RelayCommand(_ => MessageBox.Show("Start"));
+      public ICommand StopCommand { get; } = new RelayCommand(_ => MessageBox.Show("Stop"));
+
+      [NotifyPropertyChangedInvocator]
+      protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+      {
+         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+      }
+   }
+}
