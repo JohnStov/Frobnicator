a3d799d5aebb29b032dce35b0b922625e67213ba
John Stovin <john.d.stovin@gmail.com>
2017-02-12 15:56:02 +0000

    Stream tests

---
+++ StreamAudio.Tests/StreamsTests.fs
@@ -0,0 +1,22 @@
+﻿namespace StreamAudio.Tests
+
+open NUnit.Framework
+
+[<TestFixture>]
+type StreamsTests() = 
+
+    [<Test>]
+    member this.``sine creates a time-varying sequence``() =
+        let sine = Streams.sine 44100.0 440.0
+        let pairs = sine |> Seq.pairwise |> Seq.take 100
+        Assert.IsTrue(pairs |> Seq.forall (fun (x, y) -> x <> y))
+    
+    [<Test>]
+    member this.``maximum sine amplitude is 1.0``() =
+        let sine = Streams.sine 44100.0 440.0 |> Seq.take 1000
+        Assert.IsTrue(sine |> Seq.forall (fun x -> x <= 1.0))
+
+    [<Test>]
+    member this.``minimum sine amplitude is -1.0``() =
+        let sine = Streams.sine 44100.0 440.0 |> Seq.take 1000
+        Assert.IsTrue(sine |> Seq.forall (fun x -> x >= -1.0))
