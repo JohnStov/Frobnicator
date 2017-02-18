5795f93520a3c878ae0be7dd023730212b470210
John Stovin <john.d.stovin@gmail.com>
2017-02-18 16:05:53 +0000

    Unit Tests

--- Frobnicator.Tests/ViewModels/PlaybackViewModelTests.cs
+++ Frobnicator.Tests/ViewModels/PlaybackViewModelTests.cs
@@ -1,4 +1,5 @@
-﻿using Frobnicator.ViewModels;
+﻿using System.Reactive.Subjects;
+using Frobnicator.ViewModels;
 using NSubstitute;
 using NUnit.Framework;
 
@@ -18,34 +19,13 @@ namespace Frobnicator.Tests.ViewModels
         }
 
         [Test]
-        public void StartCommandCanExecuteCallsIsRunning()
-        {
-            var device = Substitute.For<AudioOutput.IPlaybackDevice>();
-            var vm = new PlaybackViewModel(device);
-
-            vm.StartCommand.CanExecute(null);
-            device.Received().IsPlaying();
-        }
-
-        [Test]
         public void StopCommandCallsStop()
         {
             var device = Substitute.For<AudioOutput.IPlaybackDevice>();
-            device.IsPlaying().Returns(true);
             var vm = new PlaybackViewModel(device);
 
             vm.StopCommand.Execute(null);
             device.Received().Stop();
-        }
-
-        [Test]
-        public void StopCommandCanExecuteCallsIsRunning()
-        {
-            var device = Substitute.For<AudioOutput.IPlaybackDevice>();
-            var vm = new PlaybackViewModel(device);
-
-            vm.StopCommand.CanExecute(null);
-            device.Received().IsPlaying();
         }
     }
 }
